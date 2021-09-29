b59a4f0575bd147e586bdc60e9891e14bcc8cff3
Suman Chapai <sumanchapai@gmail.com>
2021-09-28 23:00:11 -0500

    update theme

---
+++ .updatetheme.sh
@@ -0,0 +1,6 @@
+cd "$(tutor config printroot)/env/build/openedx/themes/bloom-ed"
+git pull
+tutor images build openedx
+tutor local settheme bloom-ed $(tutor config printvalue LMS_HOST) $(tutor config printvalue CMS_HOST)
+echo "restarting openedx"
+tutor local quickstart
