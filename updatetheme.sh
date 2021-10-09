fc974e2ef1562ea0b07d8b564c30cb52c1af254a
Suman Chapai <sumanchapai@gmail.com>
2021-10-09 12:06:13 -0500

    proper comment format

--- updatetheme.sh
+++ updatetheme.sh
@@ -4,6 +4,6 @@ git pull
 tutor images build openedx
 tutor local settheme bloom-ed $(tutor config printvalue LMS_HOST) $(tutor config printvalue CMS_HOST)
 echo "restarting openedx"
-"Change Working Direcotry to the previously working directory
+# Change Working Direcotry to the previously working directory
 cd - 
 ./quickstart.sh
